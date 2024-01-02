1a65a787bd2bf22736c4e3f2b720d09cbdaf22e0
Nodar <nodo.gogaladze2002@gmail.com>
2024-01-03 01:51:10 +0400

    product table test. product model.  product table data fixture. develouping the product factory

--- ecommerce/tests/factories.py
+++ ecommerce/tests/factories.py
@@ -15,4 +15,29 @@ class CategoryFactory(factory.django.DjangoModelFactory):
     name = factory.Sequence(lambda n: "cat_name_%d" % n)   #fake.lexify(text = "cat_name_?????")     
     slug = fake.lexify(text = "cat_name_?????")
     
+
+
+
+class ProductFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Product
+        
+    web_id = factory.Sequence(lambda n: "web_id_%d" % n)
+    slug = fake.lexify(text = "prod_slug_??????")
+    name = fake.lexify(text = "prod_name_??????")
+    description = fake.text()
+    is_active = True
+    created_at = "21-09-04 22:14:18.279092"
+    updated_at = "21-09-04 22:14:18.279092"
+    
+    @factory.post_generation
+    def category(self, create, extracted, **kwargs):
+        if not create or not extracted:
+            return
+        
+        if extracted:
+            for cat in extracted:
+                self.category.add(cat)
+                
 register(CategoryFactory)
+register(ProductFactory)
